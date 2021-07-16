dbef2d28b671d57dc5e2543c5e9aaf2b74237294
Caio Garcia <52222678+caiogarciap@users.noreply.github.com>
2021-07-16 20:51:47 -0300

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 //Vest Search
 // API Key - https://developers.google.com/custom-search/v1/using_rest
-export const API_KEY = 'AIzaSyA-Hcv3ISKVSdFH-mY82e-_O2xtcUmfHAo'
+export const API_KEY = 'AIzaSyAR_iilbEoBykMJzQTV639f1GgsoFI1H80'
 
 // Context Key - https://cse.google.com/cse/create/new
 export const CONTEXT_KEY = '392f262b8a4137381'
